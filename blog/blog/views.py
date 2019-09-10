c5c4d1047fe2c86a39a06ba262b706b00e1a79f0
hb <1352464600@qq.com>
2019-09-10 10:30:26 +0800

    个人博客

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def test(request):
     return HttpResponse('tset页面')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def listpic(request):
+    return render(request, 'listpic.html')
+
+
+def newslistpic(request):
+    return render(request, 'newslistpic.html')
